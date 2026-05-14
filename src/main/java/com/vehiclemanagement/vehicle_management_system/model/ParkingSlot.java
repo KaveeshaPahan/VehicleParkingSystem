f522db612a17ac331b8a9278620f400d346d42ef
IT25103057 <IT25103057@my.sliit.lk>
2026-05-14 20:53:52 +0530

    Add toLine method
    This method converts a ParkingSlot object into one single line of text to save in a file

--- src/main/java/com/vehiclemanagement/vehicle_management_system/model/ParkingSlot.java
+++ src/main/java/com/vehiclemanagement/vehicle_management_system/model/ParkingSlot.java
@@ -40,6 +40,7 @@ public class ParkingSlot extends BaseEntity {
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 
+    //toLine method
     @Override
     public String toLine() {
         return String.join(SEP,
